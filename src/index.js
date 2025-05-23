8edecc36de12c48cd872d9168a0acb20fdb0690f
VuVietCuong <cuonghaiduongg2004@gmail.com>
2025-05-23 11:10:29 +0700

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const { default: mongoose } = require('mongoose');
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const encodedPassword = encodeURIComponent(process.env.MONGO_PASS);
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.dnrabjd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+    
+    .then(() => {
+        console.log('Connect DB success!');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+    
 
 app.get('/', (req, res) => {
     res.send('Hello World!, I am a simple Express server!');
